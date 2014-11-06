acd546cea9cea43b1661040eaccd55f2a3fd99b7
Max Menges <mmenges@mathphys.fsk.uni-heidelberg.de>
2014-11-06 10:55:20 +0100

    hmm stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
         LatexToPdf.escape_latex(text)
     end
 
+    def tex2html(text)
+        html = text
+        html.gsub!("\r", '<br>')
+        html.gsub!("\\begin{itemize}", "<ul>")
+        html.gsub!("\\item", "</li><li>")
+        html.gsub!("\\end{itemize}", "</li></ul>")
+
+        html.html_safe
+    end
+
 end
